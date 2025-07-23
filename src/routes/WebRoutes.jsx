d712925f957a00e83c7f406470faf40f3122ff2e
RajRanipa <rajranipa36@gmail.com>
2025-07-23 17:06:34 +0530

    Fix: Scroll add to rout file

--- src/routes/WebRoutes.jsx
+++ src/routes/WebRoutes.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/web/Home';
-import About from '../pages/web/About';
 import Products from '../pages/web/Products';
 import Careers from '../pages/web/Careers';
 import Contact from '../pages/web/Contact';
 import Login from '../pages/auth/Login';
 import Signup from '../pages/auth/Signup';
 import PageTransition from '../components/PageTransition';
-// import ProtectedRoute from '../components/ProtectedRoute';
 import WebLayout from '../layouts/WebLayout';
 import AuthLayout from '../layouts/AuthLayout';
 import Board from '../components/products/Board';
@@ -16,48 +14,34 @@ import Bulk from '../components/products/Bulk';
 import Module from '../components/products/Module';
 import Paper from '../components/products/Paper';
 import Blanket from '../components/products/Blanket';
+import { ScrollObserverProvider } from '../context/ScrollObserverContext';
 
 const WebRoutes = () => {
   return (
-    <Routes>
-      {/* Public pages */}
-      <Route element={<WebLayout />}>
-        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
-        {/* <Route path="/products" element={<PageTransition><Products /></PageTransition>} /> */}
-        <Route path="/products/*" element={<Products />}>
-          <Route index element={<PageTransition><Blanket /></PageTransition>} />
-          <Route path="ceramic-fiber-blanket" element={<PageTransition><Blanket /></PageTransition>} />
-          <Route path="ceramic-fiber-board" element={<PageTransition><Board /></PageTransition>} />
-          <Route path="ceramic-fiber-bulk" element={<PageTransition><Bulk /></PageTransition>} />
-          <Route path="ceramic-fiber-module" element={<PageTransition><Module /></PageTransition>} />
-          <Route path="ceramic-fiber-paper" element={<PageTransition><Paper /></PageTransition>} />
+    <ScrollObserverProvider>
+      <Routes>
+        {/* Public pages */}
+        <Route element={<WebLayout />}>
+          <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+          {/* <Route path="/products" element={<PageTransition><Products /></PageTransition>} /> */}
+          <Route path="/products/*" element={<Products />}>
+            <Route index element={<PageTransition><Blanket /></PageTransition>} />
+            <Route path="ceramic-fiber-blanket" element={<PageTransition><Blanket /></PageTransition>} />
+            <Route path="ceramic-fiber-board" element={<PageTransition><Board /></PageTransition>} />
+            <Route path="ceramic-fiber-bulk" element={<PageTransition><Bulk /></PageTransition>} />
+            <Route path="ceramic-fiber-module" element={<PageTransition><Module /></PageTransition>} />
+            <Route path="ceramic-fiber-paper" element={<PageTransition><Paper /></PageTransition>} />
+          </Route>
+          <Route path="/career" element={<PageTransition><Careers /></PageTransition>} />
+          <Route path="/contact" element={<PageTransition><Contact /></PageTransition>} />
         </Route>
-        <Route path="/career" element={<PageTransition><Careers /></PageTransition>} />
-        <Route path="/contact" element={<PageTransition><Contact /></PageTransition>} />
-      </Route>
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<PageTransition><Login /></PageTransition>} />
-        <Route path="/signup" element={<PageTransition><Signup /></PageTransition>} />
-      </Route>
-    </Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<PageTransition><Login /></PageTransition>} />
+          <Route path="/signup" element={<PageTransition><Signup /></PageTransition>} />
+        </Route>
+      </Routes>
+    </ScrollObserverProvider>
   );
 };
 
-// import { Routes, Route } from 'react-router-dom';
-
-
-// const WebRoutes = () => (
-//   <Routes>
-//     <Route element={<WebLayout />}>
-//       <Route path="/" element={<PageTransition><Home /></PageTransition>} />
-//       <Route path="/about" element={<PageTransition><About /></PageTransition>} />
-//       {/* other web pages */}
-//     </Route>
-
-//     <Route element={<AuthLayout />}>
-//       <Route path="/login" element={<Login />} />
-//       <Route path="/signup" element={<Signup />} />
-//     </Route>
-//   </Routes>
-// );
 export default WebRoutes;
