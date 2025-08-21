6c0bf34ecbeb934e6e83bbd665fbaa8ca3328163
RajRanipa <rajranipa36@gmail.com>
2025-08-21 18:14:20 +0530

    sure

--- src/components/LogInOut.jsx
+++ src/components/LogInOut.jsx
@@ -14,7 +14,7 @@ const LogInOut = ({ variant = 'header', className = '' }) => {
 
   return (
     <a
-      onClick={() => navigate('https://erp.orientfibertech.com/login')}
+      href='https://erp.orientfibertech.com/login'
       className={`${combinedStyle} flex items-center gap-2 px-4 py-2 rounded-md`}
       aria-label="Log in"
       id="loginout_button"
